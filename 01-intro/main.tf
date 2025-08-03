3abb1b3507e624c37e35f089fa55382dd9255cec
kishor <kishorebsc12@gmail.com>
2025-08-03 11:52:41 +0530

    added

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -17,7 +17,31 @@ output "x_value" {
     value = var.x
 }
 
+#combination of strings use "${var.x}"
 output "x_value" {
     value = "value of x ${var.x}" 
 }
 
+#List variable declaration
+variable "y" {
+  default = ["a","1","0.3","true"]
+}
+
+#List output
+output "y_value" {
+    value = var.y[0]
+}
+
+#Map variable declaration
+variable "z" {
+    default = {
+        "name" = "kishor reddy"
+        "age"  = 30
+        "city" = "hyderabad"
+    } 
+}
+
+#Map output
+output "z_value" {
+value = "Name is ${var.z.name} and age is ${var.z[age]}"
+}
