fb125ce298816910a00d920f8b78d037d87c0776
kishor <kishorebsc12@gmail.com>
2025-08-03 11:41:12 +0530

    added

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -6,3 +6,14 @@ resource "local_file" "foo" {
   content = "Hello, World!"
   filename= "/tmp/hello.txt"  
 }
+
+# normal variable declaration
+variable "x" {
+default = 10
+}
+
+#print output
+output "x_value" {
+    value = var.x
+}
+
